4dc44e875b0ef2a13258b43ae0205b0031e9cfe5
Trent21 <tmitch.wall@gmail.com>
2019-06-21 16:32:21 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+	jQuery(".amader-slider").owlCarousel({
+		'autoPlay' : 3000,
+		'items' : 3,
+		'itemsDesktop' : [1200, 3],
+		'itemsDesktopSmall' : [1000, 3],
+		'itemsTablet' : [768, 2],
+		'stopOnHover' : true,
+	});
+	
+	jQuery(".amader-slider2").owlCarousel({
+		'autoPlay' : 3000,
+		'items' : 3,
+		'itemsDesktop' : [1200, 3],
+		'itemsDesktopSmall' : [1000, 3],
+		'itemsTablet' : [768, 2],
+		'stopOnHover' : true,
+	});
+
+	
+});
